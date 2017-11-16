94f2e4a34f06c5ed781765dcc54d3e83873ec2c9
Jochen Metzger <j.metzger@t3company.de>
2017-11-16 10:55:56 +0100

    Only trainer metzger has currently

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello NEW world in feature-4714");
+		System.out.println("ABC Hello NEW world in feature-4714");
 
 	}
 
