594316d6f4b9310fa7e050657edab3eb237ce617
Jochen Metzger <j.metzger@t3company.de>
2017-11-16 11:40:40 +0100

    Made changes in feature 2

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("feature change -> Hello NEW world in feature-2");
+		System.out.println("Test that feature change -> Hello NEW world in feature-2");
 
 	}
 
